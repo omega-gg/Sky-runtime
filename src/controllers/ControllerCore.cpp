54ba74c73778e303a2fbe3291d28de89897766b1
bunjee <bunjee@omega.gg>
2026-04-17 18:56:10 +0200

    Sky 3.1.0-0

--- src/controllers/ControllerCore.cpp
+++ src/controllers/ControllerCore.cpp
@@ -127,7 +127,7 @@ W_INIT_CONTROLLER(ControllerCore)
 // Static variables
 
 // NOTE: Also check DataLocal_patch, version_windows.
-static const QString CORE_VERSION = "3.0.0-2";
+static const QString CORE_VERSION = "3.1.0-0";
 
 static const int CORE_CACHE        = 1048576 * 100; // 100 megabytes
 static const int CORE_CACHE_PIXMAP = 1048576 *  30; //  30 megabytes
